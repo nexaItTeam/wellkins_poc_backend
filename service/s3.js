59d6b43b8f4ea7b4874e2c88c34a7f988240d77e
nexaItTeam <itsupport@arctechz.com>
2023-07-24 15:23:35 +0530

    bucket name insert

--- service/s3.js
+++ service/s3.js
@@ -3,7 +3,7 @@ const { S3 } = require('aws-sdk')
 exports.imageUpload = async (file) => {
     const s3 = new S3()
     const params = {
-        Bucket: welkins-capital-bucket,
+        Bucket: "welkins-capital-bucket",
         Key: `property/${file.originalname}`,
         Body: file.buffer,
         ContentType: 'image/png',
